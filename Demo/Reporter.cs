72cd2df06a61c51ff0e1fe66fd78a95e47c5e986
Manoj Yadav <manuvns.manoj@gmail.com>
2020-05-10 18:40:23 +0530

    Add files via upload

---
+++ Demo/Reporter.cs
@@ -0,0 +1,58 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIDemo
+{
+    public static class Reporter
+    {
+        public static ExtentReports extentReports;
+        //public static ExtentReports extent;
+        public static ExtentHtmlReporter htmlReporter;
+        public static ExtentTest testCase;
+
+        public static void SetupExtentReport(string reportName, string documentTitle, dynamic path)
+        {
+            htmlReporter = new ExtentHtmlReporter(path);
+            htmlReporter.Config.Theme = Theme.Standard;
+            htmlReporter.Config.DocumentTitle = documentTitle;
+            htmlReporter.Config.ReportName = reportName;
+
+            extentReports = new ExtentReports();
+            extentReports.AttachReporter(htmlReporter);
+            //extentReports = extent;
+        }
+
+        public static void CreateTest(string testName)
+        {
+            testCase = extentReports.CreateTest(testName);
+        }
+
+        public static void LogToReport(Status status, string message)
+        {
+            testCase.Log(status, message);
+        }
+
+        public static void FlushReport()
+        {
+            extentReports.Flush();
+        }
+
+        public static void TestStatus(string status)
+        {
+            if (status.Equals("Pass"))
+            {
+                testCase.Pass("Test is passed");
+            }
+            else
+            {
+                testCase.Pass("Test is failed");
+            }
+        }
+    }
+}
